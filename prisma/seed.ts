d737531cbd6db5db95bd80c97d7870c8c3298615
AlXavi2004 <146011365+alexxcalle@users.noreply.github.com>
2024-05-01 17:23:07 -0500

    Primera tabla

---
+++ prisma/seed.ts
@@ -0,0 +1,45 @@
+import { PrismaClient } from '@prisma/client';
+
+// initialize Prisma Client
+const prisma = new PrismaClient();
+
+async function main() {
+  const post1 = await prisma.users.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      name: 'Juan Perez',
+      about: 'Mi nombre es Juan Perez y estudio programacion',
+      role: 'Estudiante',
+      email: 'juan@gmail.com',
+      password: '124325345',
+      cellphone: 9951227483,
+    },
+  });
+
+  const post2 = await prisma.users.upsert({
+    where: { id: 2 },
+    update: {},
+    create: {
+      name: 'Pedro Messi',
+      about: '',
+      role: 'Docente',
+      email: 'pedroms@gmail.com',
+      password: '1245657545',
+      cellphone: 9951576583,
+    },
+  });
+
+  console.log({ post1, post2 });
+}
+
+// execute the main function
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    // close Prisma Client at the end
+    await prisma.$disconnect();
+  });
---
+++ src/users/users.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return this.prisma.users.findMany({ where: { role: 'Docente' } });
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+}
